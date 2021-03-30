f33e4e099cd37c5c1d1fc5883bbf5a1d447ae49e
erdem <erdemgokdemir@gmail.com>
2021-03-30 18:49:07 +0300

    init

---
+++ src/main/java/org/me/testing/helloworldcontroller/HelloWorldControllerApplication.java
@@ -0,0 +1,13 @@
+package org.me.testing.helloworldcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/me/testing/helloworldcontroller/HelloWorldControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.testing.helloworldcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
